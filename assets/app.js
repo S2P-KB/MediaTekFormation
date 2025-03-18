a48e304d6750a507417ff53af9acc70c84c63778
Kevin B <kevin.banzet@gmail.com>
2025-03-18 15:46:33 +0100

    Fin première tâche.

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,5 @@
 import './bootstrap.js';
+import log from 'loglevel';
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -7,4 +8,4 @@ import './bootstrap.js';
  */
 import './styles/app.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+log.info('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
